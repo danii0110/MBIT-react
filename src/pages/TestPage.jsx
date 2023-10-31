fb90a89bbf9266be1a4a9fece49fcc39571559d5
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-10-31 20:23:56 +0900

    feat: 문제 추가

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -3,33 +3,30 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 import classes from './TestPage.module.scss';
 
-import logo from '../assets/images/logo.png';
+// import logo from '../assets/images/logo.png';
 import backBtn from '../assets/images/backBtn.png';
 import nextBtn from '../assets/images/nextBtn.png';
 import SelectBtn from '../components/SelectBtn';
 
-// import testData from './content/questions.json';
+import testData from './content/questions';
 
 const TestPage = () => {
-  // const navigate = useNavigate(); //
-  // const { params } = useParams(); //파라미터 값
+  const navigate = useNavigate(); //
+  const { params } = useParams(); //파라미터 값
 
-  // const [question, setQuestion] = useState(testData.question); //문제
-  // const [contents, setContents] = useState([]); //답변
-  // const [Number, setNumber] = useState(1); //문제 번호
+  const [question, setQuestion] = useState(testData.question); //문제
+  const [contents, setContents] = useState([]); //답변
+  const [Number, setNumber] = useState(1); //문제 번호
 
-  // const btnClickHandler = () => {};
+  const btnClickHandler = () => {};
 
   return (
     <div className={classes.container}>
-      <div className={classes.headerImage}>
-        <img src={logo} alt='logo-header' />
-      </div>
       <div className={classes.progress}>1/12</div>
       <div className={classes.question}>question</div>
-      {/* <div className={classes.answer}>answer</div> */}
-      <SelectBtn>test message</SelectBtn>
-
+      <div className={classes.answers}>
+        <SelectBtn>test message</SelectBtn>
+      </div>
       <div className={classes.btn}>
         <img className={classes.backBtn} src={backBtn} alt='back-button' />
         <img className={classes.nextBtn} src={nextBtn} alt='next-button' />
