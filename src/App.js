5c9aee1900ed3a9712558b5fb432fd88c98b554e
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-11-05 20:35:29 +0900

    feat: 시작 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import StartPage from './StartPage';
 import TestPage from './pages/TestPage';
+import Main from './pages/Main';
 // import ResultPage from './pages/ResultPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route exact path='/' element={StartPage} /> */}
+        <Route exact path='/' element={<Main />} />
         <Route path='/test/:id' element={<TestPage />} />
         {/* <Route path='/result' element={<ResultPage />} /> */}
       </Routes>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,23 +1,31 @@
-import style from "../styles/Main.module.scss";
-import logoImg from "../assets/images/indexLogo.svg";
-import Fe from "../assets/images/result(Fe).svg";
+import style from '../styles/Main.module.scss';
+import logoImg from '../assets/images/indexLogo.svg';
+import Fe from '../assets/images/result(Fe).svg';
 // 전체 결과 유형 -> 애니메이션 전환
-import startBtn from "../assets/images/startBtn.svg";
+import startBtn from '../assets/images/startBtn.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Main() {
+  const navigate = useNavigate();
+
   return (
     <div className={style.Main}>
       <div className={style.logoArea}>
-        <img src={logoImg} alt="로고" />
+        <img src={logoImg} alt='로고' />
       </div>
 
       <div className={style.resultCase}>
-        <img src={Fe} alt="결과 유형" />
+        <img src={Fe} alt='결과 유형' />
         {/* 해당 부분 프레임워크 추가해서 수정 */}
       </div>
 
-      <div className={style.startBtn}>
-        <img src={startBtn} alt="시작하기 버튼" />
+      <div
+        className={style.startBtn}
+        onClick={() => {
+          navigate('/test/1');
+        }}
+      >
+        <img src={startBtn} alt='시작하기 버튼' />
       </div>
     </div>
   );
